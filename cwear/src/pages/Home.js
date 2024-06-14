e0d634188fb7b682ff91417b175ed99135ee459a
kiswor <kisworchhetri.2001@gmail.com>
2024-06-14 14:47:39 +0545

    yoga girl section finished

--- cwear/src/pages/Home.js
+++ cwear/src/pages/Home.js
@@ -9,6 +9,23 @@ export default function Home() {
         <>
             <NavHeader />
             <Navbar />
+            <div className='container-fluid'>
+                <div className='main-1-yg'>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="p-3 mb-2 bg-light d-flex flex-column justify-content-evenly" style={{ height: "250px" }}>
+                                <span style={{ fontSize: "20px", color: "grey", fontWeight: "100" }}>New Luma Yoga Collection</span>
+                                <p style={{ fontSize: "30px", color: "#575757" }}>Get fit and look fab in new <br />
+                                    seasonal styles</p>
+                                <button class="btn btn-primary ybtn">Shop New Yoga</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
         </>
     )
 }
